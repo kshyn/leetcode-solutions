e27cc0c58e0a40af18aea9838a4a471ad3a8c3c1
kshyn <102468422+kshyn@users.noreply.github.com>
2026-07-02 18:03:20 +0300

    Time: 11 ms (36.59%) | Memory: 19.2 MB (53.01%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -1,19 +1,6 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        if x < 0:
-            return False
-        elif 0 <= x < 10 :
-            return True
-        else:
-            digit = tuple(int(char) for char in str(abs(x)))
-            left = 0          
-            right = len(digit) - 1 
-            while left < right:
-                if digit[left] == digit[right]:
-                    left += 1
-                    right -= 1
-                else:
-                    return False
-            return True
+         s = str(x)
+         return s == s[::-1]
 
 
